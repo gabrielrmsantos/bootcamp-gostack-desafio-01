9615b012beac8751d9e794ddd061c38d5533598b
Gabriel Roberto <gabriel.roberto@in8.com.br>
2020-02-20 07:32:12 -0300

    rotas do projeto / middleware para verificao de ids e contagem de requisicoes

---
+++ README.md
@@ -0,0 +1,5 @@
+# bootcamp-gostack-desafio-01
+
+NProjeto de API em NodeJS com dados mantidos em tempo de execução para cumprir com o desafio proposto no seguinte link:
+
+<a href="https://github.com/Rocketseat/bootcamp-gostack-desafio-01/blob/master/README.md#desafio-01-conceitos-do-nodejs">Desafio 01: Conceitos do NodeJS</a>
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+
+const server = express();
+const projects = [];
+let numRequests = 0;
+
+// count number of requests
+const requestCounter = (req, res, next) => {
+  numRequests++;
+  console.log('Requests counter:', numRequests);
+
+  next();
+}
+
+// verify project id
+const checkProjectId = (req, res, next) => {
+  const projectIndex = projects.findIndex(project => project.id == req.params.id);
+  if(projectIndex === -1)
+    return res.status(404).json({ error: "Project doesn't exists" });
+
+  req.projectIndex = projectIndex;
+
+  next();
+}
+
+server.use(express.json());
+server.use(requestCounter);
+
+// create a new project
+server.post('/projects', (req, res) => {
+  const projectIndex = projects.findIndex(project => project.id == req.body.id);
+  if(projectIndex != -1)
+    return res.status(403).json({ error: "Project id already exists" })
+
+  projects.push(req.body);
+
+  return res.json(projects);
+})
+
+// get all projects
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+})
+
+// update project data
+server.put('/projects/:id', checkProjectId, (req, res) => {
+  projects[req.projectIndex].title = req.body.title;
+
+  return res.json(projects);
+})
+
+// remove projects 
+server.delete('/projects/:id', checkProjectId, (req, res) => {
+  projects.splice(req.projectIndex, 1);
+
+  return res.json(projects);
+})
+
+// create tasks for projects
+server.post('/projects/:id/tasks', checkProjectId, (req, res) => {
+  const { title } = req.body;
+  projects[req.projectIndex].tasks.push(title);
+
+  return res.json(projects);
+})
+
+server.listen(3000)
